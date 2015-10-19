4c82087333b3a644f4a640b9673432f7b99c6d69
macbellingrath <macbellingrath@gmail.com>
2015-10-19 14:16:18 -0400

    Playing with camera.

--- Camera/ViewController.m
+++ Camera/ViewController.m
@@ -9,19 +9,90 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+<UINavigationControllerDelegate,UIImagePickerControllerDelegate>
+
+@property UIImagePickerController * pickerController;
+@property (weak, nonatomic) IBOutlet UIImageView *mySelfieImageView;
+- (IBAction)takePhotoButtonPressed:(UIButton *)sender;
+
+-(void)presentPicker;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    
+    self = [super initWithCoder:aDecoder];
+    
+    if (self != nil) {
+        
+       
+        
+  
+    }
+    
+    return self;
+    
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    if (self.mySelfieImageView.image == nil) {
+        [self presentPicker];
+    }
+    self.mySelfieImageView.contentMode = UIViewContentModeScaleAspectFit;
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    UIImage * image = info[UIImagePickerControllerOriginalImage];
+    self.mySelfieImageView.image = image;
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    
+}
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    
+}
+
+-(void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //Add pickercontroller's view to superview
+//    [self.view addSubview:self.pickerController.view];
+    
+
+
+}
+-(void)presentPicker{
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        
+        
+        //allocate and initialize our UIImagePickerController
+        self.pickerController = [[UIImagePickerController alloc] init];
+        
+        //Set source type to camera
+        self.pickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+        self.pickerController.cameraCaptureMode = UIImagePickerControllerCameraDeviceFront;
+        self.pickerController.delegate = self;
+        
+    } else {
+        
+        //allocate and initialize our UIImagePickerController
+        self.pickerController = [[UIImagePickerController alloc] init];
+        self.pickerController.sourceType =
+        UIImagePickerControllerSourceTypePhotoLibrary;
+        self.pickerController.delegate = self;
+        [self presentViewController: self.pickerController animated:true completion:nil];
+    }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
+- (IBAction)takePhotoButtonPressed:(UIButton *)sender {
+    [self presentPicker];
+    
+}
 @end
