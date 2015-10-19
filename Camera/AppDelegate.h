7612ef765fcfc1a23e3e25143add31b549479c73
macbellingrath <macbellingrath@gmail.com>
2015-10-19 09:25:50 -0400

    Initial Commit

---
+++ Camera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Camera
+//
+//  Created by Mac Bellingrath on 10/19/15.
+//  Copyright © 2015 Mac Bellingrath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Camera/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Camera
+//
+//  Created by Mac Bellingrath on 10/19/15.
+//  Copyright © 2015 Mac Bellingrath. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
